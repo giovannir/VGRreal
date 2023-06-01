f10b66ff286906ae1987525749b9d026931b01c7
giovannir <135184041+giovannir@users.noreply.github.com>
2023-05-31 21:07:02 -0700

    Add files via upload

--- public/contact.php
+++ public/contact.php
@@ -1,3 +1,28 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Replace these with your own values
+    $to = 'greitano6@gmail.com';
+    $subject = 'Contact Form Submission';
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               'X-Mailer: PHP/' . phpversion();
+
+    // Send the email
+    if (mail($to, $subject, $message, $headers)) {
+        // Redirect back to the contact page with a success parameter
+        header("Location: contact.php?success=1");
+        exit();
+    } else {
+        // Redirect back to the contact page with an error parameter
+        header("Location: contact.php?error=1");
+        exit();
+    }
+}
+?>
 <!DOCTYPE html>
 
 <html lang="en">
@@ -83,7 +108,7 @@
       echo '<div class="success-message"><p>Thank you for contacting us! We will get back to you shortly.</p></div>';      }
       ?>
 
-       <form action="send_email.php" method="POST">
+      <form action="contact.php" method="POST">
           <label for="name">Name:</label>
           <input type="text" id="name" name="name" required>
           
